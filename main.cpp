8a0b8b61953c961900adfbe21fb8961e3f3ccb56
adefiks <adefiks@gmail.com>
2019-02-10 01:16:48 +0100

    new

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 // main project 06.02.2019
 
 #include <iostream>
+#include <SDL2/SDL.h>
 using namespace std;
 
-int main()
+int WinMain(int argc, char *argv[])
 {
-    int x;
+    //Error Checking/Initialisation
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+        return -1;
+    }
 
-    cout << "heej" << endl;
-    cin >> x;
+    SDL_Window *window = SDL_CreateWindow("gro", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    int k;
+    cout << "heeej" << endl;
+    cin >> k;
 
     return 0;
 }
