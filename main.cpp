0f91779694069477f854a4e49ddc0931cf4eaf69
Piotr Potocki <adefiks@gmail.com>
2019-02-27 13:51:27 +0100

    s

--- main.cpp
+++ main.cpp
@@ -6,15 +6,30 @@
 
 int WinMain(int argc, char *argv[])
 {
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    uint32_t frameStart;
+    int frameTime;
     Game game;
 
     game.init("first engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
     while (game.running())
     {
+
+        frameStart = SDL_GetTicks();
+
         game.handleEvents();
         game.update();
         game.render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     game.clean();
