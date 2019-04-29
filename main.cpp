2f24cd90d249e231d46a8649aba3d35303ce346d
adefiks <adefiks@gmail.com>
2019-04-29 17:12:06 +0200

    next

--- main.cpp
+++ main.cpp
@@ -38,13 +38,3 @@ int WinMain(int argc, char *argv[])
 
     return 0;
 }
-
-// old
-// if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
-// {
-//     SDL_Window *window = SDL_CreateWindow("tytu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-//     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-//     SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-//     SDL_RenderClear(renderer);
-//     SDL_RenderPresent(renderer);
-// }
